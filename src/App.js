d215f3a0173680a4d61bc722eb8a8cc322faed2a
stephanie-garcia <86178416+stephanie-garcia@users.noreply.github.com>
2021-12-12 17:41:42 -0500

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -2,6 +2,16 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App"> Weather App </div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Stephanie Garcia and is {""}
+        <a
+          href="https://github.com/stephanie-garcia/react-weather-app"
+          target="_blank"
+        > open-sourced on GitHub </a>
+      </footer>
+    </div>
+  );
 }
-
