fef3516178ded547a724f8ffaa6db23fcdabca20
stephanie-garcia <86178416+stephanie-garcia@users.noreply.github.com>
2021-12-12 18:11:53 -0500

    installed bootstrap

--- src/App.js
+++ src/App.js
@@ -4,14 +4,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Stephanie Garcia and is {""}
-        <a
-          href="https://github.com/stephanie-garcia/react-weather-app"
-          target="_blank"
-        > open-sourced on GitHub </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Stephanie Garcia and is {""}
+          <a
+            href="https://github.com/stephanie-garcia/react-weather-app"
+            target="_blank"
+          >
+            {" "}
+            open-sourced on GitHub{" "}
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
