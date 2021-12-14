e10f333e5896692eaf50f8fa68367280f2078c4a
stephanie-garcia <86178416+stephanie-garcia@users.noreply.github.com>
2021-12-13 22:20:34 -0500

    created component for search engine

---
+++ src/WeatherInfo.js
@@ -0,0 +1,39 @@
+import React from "react";
+import FormattedDate from "./FormattedDate";
+
+export default function Weather(props) {
+  return (
+    <div className="WeatherInfo">
+      <h1>{props.data.city}</h1>
+      <ul>
+        <li>
+          {" "}
+          <FormattedDate date={props.data.date} />
+        </li>
+        <li className="text-capitalize">{props.data.description}</li>
+      </ul>
+      <div className="row mt-3">
+        <div className="col-3">
+          <div className="clearfix">
+            <img src={props.data.iconUrl} alt={props.data.description} />
+          </div>
+        </div>
+        <div className="col-3">
+          <div className="number">
+            <span className="temperature">
+              {Math.round(props.data.temperature)}
+            </span>
+            <span className="unit">°C</span>
+          </div>
+        </div>
+
+        <div className="col-6">
+          <ul>
+            <li>Humidity:{props.data.humidity}%</li>
+            <li>Wind:{props.data.wind}km/hr</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
