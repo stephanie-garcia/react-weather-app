a7984d7d7d2c921c779365381b3015f2505b6ae6
stephanie-garcia <86178416+stephanie-garcia@users.noreply.github.com>
2021-12-14 00:13:50 -0500

    fixed bugs

--- src/App.js
+++ src/App.js
@@ -6,13 +6,13 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity="New York"/>
+        <Weather defaultCity="New York" />
         <footer>
           This project was coded by Stephanie Garcia and is {""}
           <a
             href="https://github.com/stephanie-garcia/react-weather-app"
             target="_blank"
-        
+            rel="noreferrer"
           >
             {" "}
             open-sourced on GitHub{" "}
--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -27,7 +27,7 @@ export default function WeatherTemperature(props) {
     let farenheit = (props.celsius * 9) / 5 + 32;
     return (
       <div className="WeatherTemperature">
-        <span className="temperature"> {Math.round(props.celsius)}</span>
+        <span className="temperature"> {Math.round(farenheit)}</span>
         <span className="unit">
           <a href="/" onClick={showCelsius}>
             °C
