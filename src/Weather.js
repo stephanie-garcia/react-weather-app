2c187424f8eaa760ceab177ed70a75f03fda0ede
stephanie-garcia <86178416+stephanie-garcia@users.noreply.github.com>
2021-12-15 21:52:33 -0500

    updated apiUrl
    trying to fix loading screen error

--- src/Weather.js
+++ src/Weather.js
@@ -21,7 +21,7 @@ export default function Weather(props) {
   }
   function search() {
     const apiKey = "086aa37c369cc91063726cf6d82e0d6b";
-    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
   }
 
